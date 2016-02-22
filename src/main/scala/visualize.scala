543f694724ec928061b687946ba79e9e95faec8d
Panayiotis Smeros <psmeros@di.uoa.gr>
2016-02-22 16:49:07 +0100

    Read a graph from csv files.

--- src/main/scala/visualize.scala
+++ src/main/scala/visualize.scala
@@ -1,19 +1,24 @@
-/* SimpleApp.scala */
-
-
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
 
-object SimpleApp {
+object visualize {
   def main(args: Array[String]) {
-    val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
     val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+
+    //Requires 2 arguments
+    val nodesFile = sc.textFile(args(0));
+    val edgesFile = sc.textFile(args(1));
+    
+    val nodesRDD: RDD[(VertexId,String)] = nodesFile.map(line => line.split(",")).map(line => (line(0).toString.substring(1).toInt:VertexId, line(1).toString()))
+    val edgesRDD: RDD[Edge[String]] = edgesFile.map(line => line.split(",")).map(line => Edge(line(0).toString.substring(1).toInt:VertexId, line(1).toString.substring(1).toInt:VertexId, "subclass"))
+    
+    val graph = Graph(nodesRDD, edgesRDD)
+    
+    graph.triplets.collect.foreach(println)
+    //println(graph.numEdges, graph.numVertices)
   }
 }
